2b0247a0f28543279993ca43e5b7f019a8664ed5
Jud1cator <yarik200027@gmail.com>
2019-03-31 14:31:29 +0300

    Initial commit

---
+++ desktop/src/com/by/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.by.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.by.games.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}
